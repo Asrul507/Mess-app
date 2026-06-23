e5027340fd7f9b6e0cd61d90f6d2af8bae2c3511
asrul <asrulmuh073@gmail.com>
2026-06-24 06:24:17 +0800

    Add modular app initializer

---
+++ js/app-init.js
@@ -0,0 +1,23 @@
+function initNavigation() {
+  document.querySelectorAll('.nav-btn').forEach((button) => {
+    button.addEventListener('click', () => showPage(button.dataset.page, button.textContent));
+  });
+}
+
+function initApp() {
+  if ($('todayText')) $('todayText').textContent = formatDate();
+
+  state.purposes = Array.from(new Set([...DEFAULT_PURPOSES, ...state.purposes]));
+  saveData(STORAGE_KEYS.purposes, state.purposes);
+
+  initNavigation();
+  initRoomsMenu();
+  initEmployeesMenu();
+  initCheckinMenu();
+  initMealsMenu();
+  initReportsMenu();
+
+  renderAll();
+}
+
+initApp();
